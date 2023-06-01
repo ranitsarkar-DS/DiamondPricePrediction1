87b70357161a0dd9773fdb855438e73d395c9e66
Ranit Sarkar <ranitsarkar210902@gmail.com>
2023-06-01 12:43:10 +0530

    Model trainer

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
 import os
 import sys
 import pickle
-import numpy as np
+import numpy as np 
 import pandas as pd
-
 from src.exception import CustomException
 from src.logger import logging
 
@@ -13,35 +12,44 @@ def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path,exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj,file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
-    def evaluate_model(X_train,y_train,X_test,y_test,models):
-        try:
-            report={}
-            for i in range(len(models)):
-                model=list(models.values())[i]
-                # Train model
-                model.fit(X_train,y_train)
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
 
+            
 
-                # Predict Testing data
-                y_test_pred= model.predict(X_test)
+            # Predict Testing data
+            y_test_pred =model.predict(X_test)
 
-                #Get R2 scores ffor train and test data
-                #train_model_score = r2_score(y_train,y_test_pred)
-                test_model_score = r2_score(y_test,y_test_pred)
+            # Get R2 scores for train and test data
+            #train_model_score = r2_score(ytrain,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
 
-                report[list(models.keys())[i]]=test_model_score
+            report[list(models.keys())[i]] =  test_model_score
 
-            return report
-        
-        except Exception as e:
+        return report
+    
+    except Exception as e:
             logging.info('Exception occured during model training')
             raise CustomException(e,sys)
+    
 
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
